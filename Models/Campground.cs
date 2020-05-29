25d4fb6292ce7287abd74328894acbcc2e266bc6
James Hall-Treworgy <llahwj33@gmail.com>
2020-05-28 22:44:50 -0400

    Converted CSS to SCSS and created a style structure.

--- Models/Campground.cs
+++ Models/Campground.cs
@@ -2,6 +2,7 @@ namespace Campers.Models
 {
     public class Campground
     {
+        public string Id { get; set; } = "123";
         public string Title { get; set; }
         public string Description { get; set; }
         public string Image { get; set; }
