5a980219154f3f35eb84996239f2b0c385fbc400
James Hall-Treworgy <jameshall-treworgy@Jamess-MBP.lan>
2021-03-03 20:00:19 -0500

    Campground calls now pass auth token.

--- Services/CampgroundService.cs
+++ Services/CampgroundService.cs
@@ -1,6 +1,6 @@
+using System;
 using System.Collections.Generic;
-using System.Net.Http;
-using System.Net.Http.Json;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Campers.Models;
 using Campers.Services.Interfaces;
@@ -9,32 +9,41 @@ namespace Campers.Services
 {
     public class CampgroundService : ICampgroundService
     {
-        private HttpClient _http;
+        
+        private IBaseHttp _http;
+        private readonly JsonSerializerOptions jsonOptions = new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
 
-        public CampgroundService(HttpClient http)
+        public CampgroundService(IBaseHttp http)
         {
             _http = http;
         }
 
         public async Task<Campground> CreateCampground(Campground newCampground)
         {
-            var response = await _http.PostAsJsonAsync<Campground>("https://localhost:6001/api/campgrounds", newCampground);
-            var createdCampground = await response.Content.ReadFromJsonAsync<Campground>();
+            var response = await _http.Post<Campground>("/api/campgrounds", newCampground);
+            var createdCampground = JsonSerializer.Deserialize<Campground>(response, jsonOptions);
 
             return createdCampground;
         }
 
         public async Task<List<Campground>> GetAll()
         {
-            var campgrounds = await _http.GetFromJsonAsync<List<Campground>>("https://localhost:6001/api/campgrounds");
+            var response = await _http.Get("/api/campgrounds");
+            var campgrounds = JsonSerializer.Deserialize<List<Campground>>(response, jsonOptions);
 
             return campgrounds;
         }
 
         public async Task<Campground> GetById(int id)
         {
-            var url = $"https://localhost:6001/api/campgrounds/{id}";
-            var campground = await _http.GetFromJsonAsync<Campground>(url);
+            var url = $"/api/campgrounds/{id}";
+            var response = await _http.Get(url);
+            var campground = JsonSerializer.Deserialize<Campground>(response, jsonOptions);
+
 
             return campground;
         }
