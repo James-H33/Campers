574b07e73f40243464f09d1cf8be7aa77d699409
James Hall-Treworgy <jameshall-treworgy@Jamess-MBP.lan>
2021-03-14 14:29:07 -0400

    Simplified functionality and increased image quality for cropped images.

--- wwwroot/js/crop.js
+++ wwwroot/js/crop.js
@@ -3,30 +3,35 @@ document.addEventListener('DOMContentLoaded', () => {
 
   window.InitCropper = (imageUrl) => {
     const div = document.querySelector('#image-crop');
+    const formContainer = document.querySelector('.create-campground-page');
+    const cropperWidth = formContainer.getBoundingClientRect().width;
+    const cropperHeight = cropperWidth * .764;
 
-    window.Cropper = new Croppie(div, {
-      viewport: { width: 390, height: 300 },
-      boundary: { width: 400, height: 400 },
-      showZoomer: false,
-      enableOrientation: false
-    });
+    if (!window.Cropper) {
+      window.Cropper = new Croppie(div, {
+        viewport: { width: cropperWidth - 20, height: cropperHeight - 20 },
+        boundary: { width: cropperWidth, height: cropperHeight },
+        showZoomer: true,
+        enableOrientation: false
+      });
 
-    const onLeaveImageArea = () => {
-      window.Cropper.result('base64').then(function(blob) {
-        window.Cropper.finalResult = blob;
+      window.Cropper.GetFinalResult = () => {
+        return new Promise((resolve) => {
+          window.Cropper.result({ type: 'base64', size: { width: 800, height: 600 } }).then((blob) => {
+            window.Cropper.destroy();
+            window.Cropper = null;
+            resolve(blob);
+          });
+        });
+      }
+
+      window.Cropper.bind({
+        url: imageUrl
+      });
+    } else {
+      window.Cropper.bind({
+        url: imageUrl
       });
     }
-
-    window.Cropper.GetFinalResult = () => {
-      div.removeEventListener('mouseleave', onLeaveImageArea, false);
-      window.Cropper.destroy();
-      return window.Cropper.finalResult;
-    };
-
-    window.Cropper.bind({
-      url: imageUrl
-    });
-
-    div.addEventListener('mouseleave', onLeaveImageArea, false);
   }
 });
